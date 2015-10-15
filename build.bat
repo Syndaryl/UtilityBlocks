21cc7dda441d7f4ea197a0abc6cc49e016e41df2
Syndaryl <emily.smirle@gmail.com>
2015-10-15 17:02:05 -0400

    Update build to 1.7

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 call gradlew build
-FOR /F "delims=" %%I IN ('DIR D:\Games\Forge_Eclipse_workspace\AnimalsDropBones\build\libs /B /O:-D') DO echo %%I&call XCOPY D:\Games\Forge_Eclipse_workspace\AnimalsDropBones\build\libs\%%I D:\Games\.Minecraft1.8.0\mods\1.8& GOTO :EXIT
+FOR /F "delims=" %%I IN ('DIR build\libs *-universal.jar /B /O:-D') DO echo %%I&call XCOPY build\libs\%%I D:\Games\.Minecraft1.8.0\mods\1.8& GOTO :EXIT
 :EXIT
 pause
