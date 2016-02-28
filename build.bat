846b4df1cb9157d81f5f49f27dcaefae9cebe1ed
Syndaryl <emily.smirle@gmail.com>
2016-02-28 15:23:37 -0500

    Fix build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @echo off
 call gradlew build
+FOR /F "delims=" %%I IN ('DIR build\libs\*-universal.jar /B /O:-D') DO echo %%I&call XCOPY build\libs\%%I D:\Games\.Minecraft1.8.9\mods\1.8.9& GOTO :EXIT
 :EXIT
 pause
